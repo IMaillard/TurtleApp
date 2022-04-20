b39fb771b77480e6d5846b91ec41cd2ba8e67a2e
Cesar Meza <cesar.me8957@vusd.us>
2022-04-20 14:51:59 -0700

    work in progress 4/20

--- app/src/main/java/com/example/turtleapp/MainActivity.java
+++ app/src/main/java/com/example/turtleapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_yume);
 
         Button calendarButton = (Button) findViewById(R.id.calendarButton);
         calendarButton.setOnClickListener(new View.OnClickListener() {
@@ -24,13 +26,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-       /* Button alarmButton = (Button) findViewById(R.id.alarm_button);
+        Button alarmButton = (Button) findViewById(R.id.alarm_button);
         alarmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, .class));
+                startActivity(new Intent(MainActivity.this, AlarmActivity.class));
             }
-        });*/
+        });
 
         Button infoButton = (Button) findViewById(R.id.info_button);
         infoButton.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +42,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ImageButton rightArrow = (ImageButton) findViewById(R.id.right_arrow);
+        ImageButton leftArrow = (ImageButton) findViewById(R.id.left_arrow);
+
+        ImageView turtleImage = (ImageView)findViewById(R.id.imageView);
+
+        rightArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int i = 0;
+                while(i<TurtleDB.ids.length){
+turtleImage.setImageResource(TurtleDB.ids[i]);
+                }
+                i++;
+            }
+        });
+
+
     }
+
+
 
 
 
